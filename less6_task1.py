b165f552612e31bfe4bf6c292b9a247c44eb91fc
OlegPrytula <mexmat87@gmail.com>
2022-02-21 00:02:36 +0200

    lesson4_task3

--- less6_task1.py
+++ less6_task1.py
@@ -1,7 +1,7 @@
 str1 = input()
 new_list = str1.split()
 new_set = set(new_list)
-dict1 = {i : 1 for i in new_set }
+dict1 = {i: new_list.count(i) for i in new_set}
 print(dict1)
 
 # from collections import Counter
