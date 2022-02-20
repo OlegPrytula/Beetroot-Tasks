2348e0ad2ffa64a64a25995faf6e785013a1f72f
OlegPrytula <mexmat87@gmail.com>
2022-02-21 00:04:47 +0200

    mistake corrected

--- less6_task2.py
+++ less6_task2.py
@@ -17,15 +17,17 @@ def sum_of_values(dictionary):
     l = []
     for i in dictionary:
         l.append(dictionary[i])
+    print(l)
     return sum(l)
+
 print(sum_of_values(prices))
 print(sum_of_values(stock))
 # another approach
 
-
-def suma(dict1):
-    values = dict1.values()
-    return sum(values)
-
-print(suma(stock))
-print(suma(prices))
+#
+# def suma(dict1):
+#     values = dict1.values()
+#     return sum(values)
+#
+# print(suma(stock))
+# print(suma(prices))
